04479618dd4dc0a433ba9a36f3792327f037a3ea
= <=>
2022-06-24 17:46:21 -0300

    Instanciate the entities of the game and remove the title file

--- src/js/stage/play.js
+++ src/js/stage/play.js
@@ -1,22 +1,39 @@
-import { Stage, game, ColorLayer, BitmapText  } from 'melonjs/dist/melonjs.module.js';
+import {
+  Stage,
+  game,
+  state,
+  ColorLayer,
+  input,
+} from "melonjs/dist/melonjs.module.js";
+import EnemyEntity from "../renderables/enemy";
 
+import PlayerEntity from "../renderables/player";
 class PlayScreen extends Stage {
-    /**
-     *  action to perform on state change
-     */
-    onResetEvent() {
-        // add a gray background to the default Stage
-        game.world.addChild(new ColorLayer("background", "#202020"));
+  /**
+   *  action to perform on state change
+   */
+  onResetEvent() {
+    // add a gray background to the default Stage
+    game.world.addChild(new ColorLayer("background", "#202020"));
+    this.player = new PlayerEntity();
+    this.enemy = new EnemyEntity(450, 450);
+    game.world.addChild(this.enemy, 0);
+    game.world.addChild(this.player, 1);
 
-        // add a font text display object
-        game.world.addChild(new BitmapText(game.viewport.width / 2, game.viewport.height / 2,  {
-            font : "PressStart2P",
-            size : 4.0,
-            textBaseline : "middle",
-            textAlign : "center",
-            text : "Hello World !"
-        }));
-    }
-};
+    input.bindKey(input.KEY.LEFT, "left");
+    input.bindKey(input.KEY.RIGHT, "right");
+    input.bindKey(input.KEY.A, "left");
+    input.bindKey(input.KEY.D, "right");
+
+    state.change(state.PLAY);
+  }
+
+  onDestroyEvent() {
+    input.unbindKey(input.KEY.LEFT);
+    input.unbindKey(input.KEY.RIGHT);
+    input.unbindKey(input.KEY.A);
+    input.unbindKey(input.KEY.D);
+  }
+}
 
 export default PlayScreen;
