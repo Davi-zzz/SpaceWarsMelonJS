2a773b6a1a9de37c8c0c0efcf27bed46cc10be55
= <=>
2022-07-18 20:54:27 -0300

    Instanciate the EnemyManager into the game & bind the shoot button and ➕

--- src/js/stage/play.js
+++ src/js/stage/play.js
@@ -5,7 +5,7 @@ import {
   ColorLayer,
   input,
 } from "melonjs/dist/melonjs.module.js";
-import EnemyEntity from "../renderables/enemy";
+import EnemyManager from "../managers/enemy-manager";
 
 import PlayerEntity from "../renderables/player";
 class PlayScreen extends Stage {
@@ -16,14 +16,21 @@ class PlayScreen extends Stage {
     // add a gray background to the default Stage
     game.world.addChild(new ColorLayer("background", "#202020"));
     this.player = new PlayerEntity();
-    this.enemy = new EnemyEntity(450, 450);
-    game.world.addChild(this.enemy, 0);
+    // this.enemy = new EnemyEntity(140, 140);
+    this.EnemyManager = new EnemyManager();
+    this.EnemyManager.createEnemies();
+
     game.world.addChild(this.player, 1);
+    game.world.addChild(this.EnemyManager, 2);
+    // game.world.addChild(this.enemy, 2);
+
+
 
     input.bindKey(input.KEY.LEFT, "left");
     input.bindKey(input.KEY.RIGHT, "right");
     input.bindKey(input.KEY.A, "left");
     input.bindKey(input.KEY.D, "right");
+    input.bindKey(input.KEY.SPACE, "shoot", true);
 
     state.change(state.PLAY);
   }
@@ -33,6 +40,7 @@ class PlayScreen extends Stage {
     input.unbindKey(input.KEY.RIGHT);
     input.unbindKey(input.KEY.A);
     input.unbindKey(input.KEY.D);
+    input.unbindKey(input.KEY.SPACE);
   }
 }
 
